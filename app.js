225567271f9365d9315c07026b95578eab4e8a10
Melksedeque <freelancer@melksedeque.com.br>
2025-03-10 16:41:51 -0300

    Correção de url das chamadas

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   res.render("home/index");
 });
 
+app.get("/formulario", (req, res) => {
+  res.render("admin/form_add_noticia");
+});
+
+app.get("/noticias", (req, res) => {
+  res.render("noticias/noticias");
+});
+
 app.listen(3000, () => {
   console.log("Server is running with Express on port 3000");
 });
